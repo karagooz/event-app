e608723fa0f7fe2b24e3967444f3a067d101e38f
karagooz <batu2919@gmail.com>
2024-02-28 22:59:49 +0300

    Theatre List

--- src/views/public/TheatrePage/pages/TheatreList.jsx
+++ src/views/public/TheatrePage/pages/TheatreList.jsx
@@ -1,21 +1,24 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import "../../../../styles/card.css";
 import { axiosInstance } from '../../../../api/axiosInstance';
 import moment from "moment";
 import "moment/locale/tr";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCalendarDays,faMapMarkerAlt,} from "@fortawesome/free-solid-svg-icons";
+import { faCalendarDays, faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
+
 function TheatreList() {
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
     axiosInstance.get('/eventList')
       .then(response => {
-        const filteredEvents = response.data.filter(item => item.category === "tiyatro");
+        const filteredEvents = response.data.filter(item => item.category === "theatre");
         setEvents(filteredEvents);
       })
       .catch(error => console.error("Veri çekme hatası:", error));
   }, []);
+
   const formattedDate = (date) => {
     return moment(date, "DD-MM-YYYY HH:mm")
       .locale("tr")
@@ -24,25 +27,28 @@ function TheatreList() {
 
   return (
     <div className="card-container">
-    {events.map((item, index) => (
-      <div className="card" key={index}>
-        <img className="card-image" src={item.image} alt={item.name} />
-        <h2 className="card-title">{item.name}</h2>
-        <span className="card-description">{item.description}</span>
-        <span className="card-date">
-          <FontAwesomeIcon icon={faCalendarDays} />
-          <br />
-          {formattedDate(item.date)}
-        </span>
-        <span className="card-location" style={{ color: 'green' }}>
-          <FontAwesomeIcon icon={faMapMarkerAlt} style={{ color: 'green' }}/>
-          <br />
-          {item.location}
-        </span>
-      </div>
-    ))}
-  </div>
+      {events.map((item, index) => (
+        <div className="card" key={index}>
+          <Link to={'/theatre/'+ item.id} className="card-link" style={{ textDecoration: 'none', color: 'inherit' }}>
+            <img className="card-image" src={item.image} alt={item.name} />
+            <h2 className="card-title">{item.name}</h2>
+          </Link>
+          <span className="card-description">{item.description}</span>
+          <span className="card-date">
+            <FontAwesomeIcon icon={faCalendarDays} />
+            <br />
+            {formattedDate(item.date)}
+          </span>
+          <span className="card-location" style={{ color: 'green' }}>
+            <FontAwesomeIcon icon={faMapMarkerAlt} style={{ color: 'green' }} />
+            <br />
+            {item.location}
+          </span>
+        </div>
+      ))}
+    </div>
   );
+  
 }
 
 export default TheatreList;
