f7a40a473a6ee6a6830129b32999c1993730ec6f
karagooz <batu2919@gmail.com>
2024-02-28 23:01:26 +0300

    Theatre Routes

--- src/views/public/TheatrePage/index.jsx
+++ src/views/public/TheatrePage/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import TheatreList from "./pages/TheatreList";
-import TheatreDetail from "./pages/TheatreList";
+import TheatreDetail from "./pages/TheatreDetail";
 
 function index() {
   return (
