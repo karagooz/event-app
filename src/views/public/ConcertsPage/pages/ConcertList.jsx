0a7887a40063d4c00d8eed049f6e311582fc17e0
karagooz <batu2919@gmail.com>
2024-02-28 23:05:37 +0300

    Concert List

--- src/views/public/ConcertsPage/pages/ConcertList.jsx
+++ src/views/public/ConcertsPage/pages/ConcertList.jsx
@@ -1,24 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { axiosInstance } from "../../../../api/axiosInstance";
-import "../../../../styles/card.css"
+import "../../../../styles/card.css";
+import { Link } from "react-router-dom";
 import moment from "moment";
 import "moment/locale/tr";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCalendarDays,faMapMarkerAlt,} from "@fortawesome/free-solid-svg-icons";
+import {
+  faCalendarDays,
+  faMapMarkerAlt,
+} from "@fortawesome/free-solid-svg-icons";
 
 function ConcertList() {
-
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
     // Axios instance kullanarak veri çekme
-    axiosInstance.get('/eventList')
-      .then(response => {
-        // Filtrasyon işlemi: Sadece "tiyatro" kategorisindeki etkinlikleri al
-        const filteredEvents = response.data.filter(item => item.category === "konser");
+    axiosInstance
+      .get("/eventList")
+      .then((response) => {
+        const filteredEvents = response.data.filter(
+          (item) => item.category === "concert"
+        );
         setEvents(filteredEvents);
       })
-      .catch(error => console.error("Veri çekme hatası:", error));
+      .catch((error) => console.error("Veri çekme hatası:", error));
   }, []);
   const formattedDate = (date) => {
     return moment(date, "DD-MM-YYYY HH:mm")
@@ -26,27 +31,28 @@ function ConcertList() {
       .format("DD MMMM dddd YYYY HH:mm");
   };
 
-
   return (
     <div className="card-container">
-        {events.map((item, index) => (
-          <div className="card" key={index}>
+      {events.map((item, index) => (
+        <div className="card" key={index}>
+          <Link to={"/concert/" + item.id} key={index} className="card-link" style={{ textDecoration: 'none', color: 'inherit' }}>
             <img className="card-image" src={item.image} alt={item.name} />
             <h2 className="card-title">{item.name}</h2>
-            <span className="card-description">{item.description}</span>
-            <span className="card-date">
-              <FontAwesomeIcon icon={faCalendarDays} />
-              <br />
-              {formattedDate(item.date)}
-            </span>
-            <span className="card-location" style={{ color: 'green' }}>
-              <FontAwesomeIcon icon={faMapMarkerAlt} style={{ color: 'green' }}/>
-              <br />
-              {item.location}
-            </span>
-          </div>
-        ))}
-      </div>
+          </Link>
+          <span className="card-description">{item.description}</span>
+          <span className="card-date">
+            <FontAwesomeIcon icon={faCalendarDays} />
+            <br />
+            {formattedDate(item.date)}
+          </span>
+          <span className="card-location" style={{ color: "green" }}>
+            <FontAwesomeIcon icon={faMapMarkerAlt} style={{ color: "green" }} />
+            <br />
+            {item.location}
+          </span>
+        </div>
+      ))}
+    </div>
   );
 }
 
